09b69f99a1f13705dd71d9bf7521ddf483d0427c
Anton Shyshko <pozernishku@gmail.com>
2022-04-15 23:50:20 +0300

    chore: black setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='xbots',
-    version='0.1.0',
-    packages=[''],
-    url='https://',
-    license='no license',
-    author='Anton Shyshko',
-    author_email='pozernishku@gmail.com',
-    description='Telegram bots stuff',
+    name="xbots",
+    version="0.1.0",
+    packages=[""],
+    url="https://",
+    license="no license",
+    author="Anton Shyshko",
+    author_email="pozernishku@gmail.com",
+    description="Telegram bots stuff",
     install_requires=[
-        'python-dotenv',
-        'pytelegrambotapi'
-    ]
+        "python-dotenv",
+        "pytelegrambotapi",
+    ],
 )
