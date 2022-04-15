702ce3624174d87c9326d510c4c5190ebeb98fc0
Anton Shyshko <pozernishku@gmail.com>
2022-04-15 23:19:20 +0300

    feat: create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='xbots',
+    version='0.1.0',
+    packages=[''],
+    url='https://',
+    license='no license',
+    author='Anton Shyshko',
+    author_email='pozernishku@gmail.com',
+    description='Telegram bots stuff'
+)
