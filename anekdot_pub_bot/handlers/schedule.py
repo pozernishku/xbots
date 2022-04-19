0d0c5d4b294945e482619ae54a8bc69732957ce2
Anton Shyshko <pozernishku@gmail.com>
2022-04-20 02:15:46 +0300

    feat: finish excercise on channel posts scheduling

---
+++ anekdot_pub_bot/handlers/schedule.py
@@ -0,0 +1,31 @@
+import os
+
+import dotenv
+import schedule
+from telebot import TeleBot
+from telebot.types import Message
+
+dotenv.load_dotenv()
+
+ANEKDOT_PUB = os.environ["ANEKDOT_PUB"]
+
+
+def send_welcome(message: Message, bot: TeleBot):
+    bot.reply_to(message, "Hi! Use /set <seconds> to set a timer")
+
+
+def set_timer(message: Message, bot: TeleBot):
+    def beep(chat_id) -> None:
+        """Send the beep message."""
+        bot.send_message(chat_id, text="Beep!")
+
+    args = message.text.split()
+    if len(args) > 1 and args[1].isdigit():
+        sec = int(args[1])
+        schedule.every(sec).seconds.do(beep, ANEKDOT_PUB).tag(ANEKDOT_PUB)
+    else:
+        bot.reply_to(message, "Usage: /set <seconds>")
+
+
+def unset_timer(message: Message, bot: TeleBot):
+    schedule.clear(message.chat.id)
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
+import threading
+import time
 
 import dotenv
+import schedule
 from telebot import TeleBot
 
 # Filter. When communicating in the Group where this bot is a member:
@@ -9,10 +12,17 @@ from telebot.custom_filters import IsAdminFilter
 # Filter. When communicating Me and Bot:
 from anekdot_pub_bot.filters.admin_filter import AdminFilter
 from anekdot_pub_bot.handlers.admin import admin_user, is_admin_user
+from anekdot_pub_bot.handlers.schedule import send_welcome, set_timer, unset_timer
 from anekdot_pub_bot.handlers.user import any_user
 
 
 def register_handlers():
+    bot.register_message_handler(
+        send_welcome, commands=["help", "start"], pass_bot=True
+    )
+    bot.register_message_handler(set_timer, commands=["set"], pass_bot=True)
+    bot.register_message_handler(unset_timer, commands=["unset"], pass_bot=True)
+
     bot.register_message_handler(
         is_admin_user, commands=["start"], is_chat_admin=True, pass_bot=True
     )
@@ -33,4 +43,10 @@ if __name__ == "__main__":
     bot.add_custom_filter(AdminFilter())
     bot.add_custom_filter(IsAdminFilter(bot))
 
-    bot.infinity_polling()
+    # bot.infinity_polling()
+    threading.Thread(
+        target=bot.infinity_polling, name="bot_infinity_polling", daemon=True
+    ).start()
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
