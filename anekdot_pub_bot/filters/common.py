0dc9b9abe76c840e6f389c1bddd849e310ee11be
Anton Shyshko <pozernishku@gmail.com>
2022-04-24 02:21:15 +0300

    refactor: extract add_custom_filters() into filters.common module

---
+++ anekdot_pub_bot/filters/common.py
@@ -0,0 +1,8 @@
+from telebot import TeleBot
+
+from anekdot_pub_bot.filters.admin_filter import AdminFilter
+
+
+def add_custom_filters(bot: TeleBot):
+    # Filter. When communicating Me and Bot:
+    bot.add_custom_filter(AdminFilter())
--- main.py
+++ main.py
@@ -5,17 +5,13 @@ import time
 import schedule
 from telebot import TeleBot
 
-# Filter. When communicating Me and Bot:
-from anekdot_pub_bot.filters.admin_filter import AdminFilter
+from anekdot_pub_bot.filters.common import add_custom_filters
 from anekdot_pub_bot.handlers.common import register_handlers
 
 if __name__ == "__main__":
     bot = TeleBot(os.environ["ANEKDOT_PUB_BOT_TOKEN"], num_threads=5)
     register_handlers(bot)
-
-    # custom filters
-    bot.add_custom_filter(AdminFilter())
-
+    add_custom_filters(bot)
     # bot.infinity_polling()
     threading.Thread(
         target=bot.infinity_polling, name="bot_infinity_polling", daemon=True
