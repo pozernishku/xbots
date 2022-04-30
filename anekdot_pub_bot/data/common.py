a1a696d5f3975f89aa9b39a126118f3ca123432d
Anton Shyshko <pozernishku@gmail.com>
2022-05-01 02:20:18 +0300

    refactor: rename beep() -> get_anekdot() in data/common module

--- anekdot_pub_bot/data/common.py
+++ anekdot_pub_bot/data/common.py
@@ -1,6 +1,6 @@
 from telebot import TeleBot
 
 
-def beep(bot: TeleBot, chat_id) -> None:
-    """Send the beep message."""
-    bot.send_message(chat_id, text="Beep!")
+def get_anekdot(bot: TeleBot, chat_id) -> None:
+    """Send the anekdot message."""
+    bot.send_message(chat_id, text="anekdot!")
--- anekdot_pub_bot/handlers/schedule.py
+++ anekdot_pub_bot/handlers/schedule.py
@@ -4,7 +4,7 @@ import schedule
 from telebot import TeleBot
 from telebot.types import Message
 
-from anekdot_pub_bot.data.common import beep
+from anekdot_pub_bot.data.common import get_anekdot
 
 ANEKDOT_PUB = os.environ["ANEKDOT_PUB"]
 
@@ -13,7 +13,7 @@ def set_timer(message: Message, bot: TeleBot):
     args = message.text.split()
     if len(args) > 1 and args[1].isdigit():
         sec = int(args[1])
-        schedule.every(sec).seconds.do(beep, bot, ANEKDOT_PUB).tag(ANEKDOT_PUB)
+        schedule.every(sec).seconds.do(get_anekdot, bot, ANEKDOT_PUB).tag(ANEKDOT_PUB)
     else:
         bot.reply_to(message, "Usage: /set <seconds>")
 
