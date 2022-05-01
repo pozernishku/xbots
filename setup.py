f007eb1cdeafe368357d18550ff643c9382487b5
Anton Shyshko <pozernishku@gmail.com>
2022-05-01 19:29:27 +0300

    chore: update setup.py -> add url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="xbots",
     version="0.1.0",
     packages=find_packages(),
-    url="https://",
+    url="https://github.com/pozernishku/xbots",
     license="no license",
     author="Anton Shyshko",
     author_email="pozernishku@gmail.com",
