49334f8d700af64da35ec1b10b2f023cda99fc36
Anton Shyshko <pozernishku@gmail.com>
2022-06-05 22:06:12 +0300

    feat: read random pdf url in get_anekdot()

--- xbots/anekdot_pub_bot/data/common.py
+++ xbots/anekdot_pub_bot/data/common.py
@@ -1,10 +1,15 @@
+import json
 import os
+import random
 
+import requests
 from telebot import TeleBot
 
-PDF_URL_LIST = os.environ["PDF_URL_LIST"]
+PDF_URL_LIST = json.loads(os.environ["PDF_URL_LIST"])
 
 
 def get_anekdot(bot: TeleBot, chat_id) -> None:
     """Send the anekdot message."""
+    pdf_url = random.choice(PDF_URL_LIST)
+    pdf = requests.get(pdf_url).content
     bot.send_message(chat_id, text="anekdot!")
