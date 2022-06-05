3d32ba88367bc22ab3c48ef599e27c6da44359a7
Anton Shyshko <pozernishku@gmail.com>
2022-06-06 00:16:43 +0300

    feat: introduce WIP -> read pdf as object with internal structure & text in data.common module

--- xbots/anekdot_pub_bot/data/common.py
+++ xbots/anekdot_pub_bot/data/common.py
@@ -1,7 +1,9 @@
 import json
 import os
 import random
+from io import BytesIO
 
+import pdfplumber
 import requests
 from telebot import TeleBot
 
@@ -11,5 +13,7 @@ PDF_URL_LIST = json.loads(os.environ["PDF_URL_LIST"])
 def get_anekdot(bot: TeleBot, chat_id) -> None:
     """Send the anekdot message."""
     pdf_url = random.choice(PDF_URL_LIST)
-    pdf = requests.get(pdf_url).content
-    bot.send_message(chat_id, text="anekdot!")
+    pdf_bytes_io = BytesIO(requests.get(pdf_url).content)
+    with pdfplumber.open(pdf_bytes_io) as pdf:
+        first_page = pdf.pages[0]
+    bot.send_message(chat_id, text=first_page.extract_text())
