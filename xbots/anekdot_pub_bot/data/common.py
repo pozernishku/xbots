8d8a66d846a17b85ad2039d4251695f8367f1c45
Anton Shyshko <pozernishku@gmail.com>
2022-06-06 22:32:11 +0300

    feat: add link to more file formats; add emotion icons in data.common.get_anekdot()

--- xbots/anekdot_pub_bot/data/common.py
+++ xbots/anekdot_pub_bot/data/common.py
@@ -22,15 +22,17 @@ def get_anekdot(bot: TeleBot, chat_id) -> None:
     pdf_page_image = random_pdf_page.to_image(resolution=150)
     # TODO: Check if it possible to save the page as pdf first and then convert to image
     #  in order to fix the red borders
-    pdf_page_text_piece = f"... {random_pdf_page.extract_text()[:200]} ..."
+    pdf_page_text_piece = f"🧩 ... {random_pdf_page.extract_text()[:200]} ..."
     pdf_page_text_piece = pdf_page_text_piece.replace("&", "&amp;")
     pdf_page_text_piece = pdf_page_text_piece.replace("<", "&lt;").replace(">", "&gt;")
     # TODO: Add direct link to pdf file (or site page, not sure) and page (if possible)
     href = f"{pdf_url}#page={random_page_n + 1}"
-    continue_reading = f'<a href="{href}">Продолжить чтение</a>'
+    continue_reading = f'<a href="{href}">📖 Продолжить чтение</a>'
+    href_file_formats = pdf_url.rsplit("/", 1)[0]
+    more_file_formats = f'<a href="{href_file_formats}">🗄️ Читать в другом формате</a>'
     bot.send_photo(
         chat_id,
         photo=pdf_page_image._repr_png_(),
-        caption=f"{pdf_page_text_piece}\n\n{continue_reading}",
+        caption=f"{pdf_page_text_piece}\n\n{continue_reading}\n{more_file_formats}",
         parse_mode="HTML",
     )
