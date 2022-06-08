c2a3c196be8c0f6289c4b2a88172e00619d467b6
Anton Shyshko <pozernishku@gmail.com>
2022-06-09 00:46:39 +0300

    docs: add TODO: Add custom filter IsURL for channel URL checking

--- xbots/anekdot_pub_bot/filters/common.py
+++ xbots/anekdot_pub_bot/filters/common.py
@@ -4,3 +4,4 @@ from telebot import custom_filters, TeleBot
 def add_custom_filters(bot: TeleBot):
     bot.add_custom_filter(custom_filters.StateFilter(bot))
     bot.add_custom_filter(custom_filters.IsDigitFilter())
+    # TODO: Add custom filter IsURL for channel URL checking
