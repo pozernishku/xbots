ea869db438c62c0409037d9f5706620a7dad4adb
Anton Shyshko <pozernishku@gmail.com>
2022-06-08 23:20:18 +0300

    feat: add skip_pending False -> True for bot.infinity_polling()

--- xbots/anekdot_pub_bot/__main__.py
+++ xbots/anekdot_pub_bot/__main__.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
     register_handlers(bot)
     add_custom_filters(bot)
     threading.Thread(target=run_pending, name="run_pending", daemon=True).start()
-    bot.infinity_polling()
+    bot.infinity_polling(skip_pending=True)
