9745dcc1bb5cbb79a996176692b48dd228de5e67
Anton Shyshko <pozernishku@gmail.com>
2022-06-09 00:59:26 +0300

    docs: add TODO: Add custom filter 'does bot is admin in your channel?'

--- xbots/anekdot_pub_bot/filters/common.py
+++ xbots/anekdot_pub_bot/filters/common.py
@@ -5,3 +5,4 @@ def add_custom_filters(bot: TeleBot):
     bot.add_custom_filter(custom_filters.StateFilter(bot))
     bot.add_custom_filter(custom_filters.IsDigitFilter())
     # TODO: Add custom filter IsURL for channel URL checking
+    # TODO: Add custom filter 'does bot is admin in your channel?'
