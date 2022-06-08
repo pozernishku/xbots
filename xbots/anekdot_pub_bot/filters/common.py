003c8417a604b882c4fb07318048cc1d8f86d5bb
Anton Shyshko <pozernishku@gmail.com>
2022-06-08 23:30:13 +0300

    feat: get rid of using AdminFilter()

--- xbots/anekdot_pub_bot/filters/common.py
+++ xbots/anekdot_pub_bot/filters/common.py
@@ -1,8 +1,5 @@
 from telebot import TeleBot
 
-from xbots.anekdot_pub_bot.filters.admin_filter import AdminFilter
-
 
 def add_custom_filters(bot: TeleBot):
-    # Filter. When communicating Me and Bot:
-    bot.add_custom_filter(AdminFilter())
+    ...
--- xbots/anekdot_pub_bot/handlers/common.py
+++ xbots/anekdot_pub_bot/handlers/common.py
@@ -10,15 +10,13 @@ from xbots.anekdot_pub_bot.handlers.user import send_welcome_any_user
 
 def register_handlers(bot: TeleBot):
     bot.register_message_handler(
-        send_welcome_admin, commands=["help", "start"], admin=True, pass_bot=True
+        send_welcome_admin, commands=["help", "start"], pass_bot=True
     )
     bot.register_message_handler(
-        send_welcome_any_user, commands=["help", "start"], admin=False, pass_bot=True
+        send_welcome_any_user, commands=["help", "start"], pass_bot=True
     )
-    bot.register_message_handler(set_timer, commands=["set"], admin=True, pass_bot=True)
-    bot.register_message_handler(
-        unset_timer, commands=["unset"], admin=True, pass_bot=True
-    )
+    bot.register_message_handler(set_timer, commands=["set"], pass_bot=True)
+    bot.register_message_handler(unset_timer, commands=["unset"], pass_bot=True)
 
 
 def run_pending():
