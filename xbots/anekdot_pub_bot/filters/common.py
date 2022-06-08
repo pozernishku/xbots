f4ab1687b75cdee1337850574e313187f3626dbe
Anton Shyshko <pozernishku@gmail.com>
2022-06-08 23:33:38 +0300

    feat: add two custom filters StateFilter(), IsDigitFilter() in filters.common module

--- xbots/anekdot_pub_bot/filters/common.py
+++ xbots/anekdot_pub_bot/filters/common.py
@@ -1,5 +1,6 @@
-from telebot import TeleBot
+from telebot import custom_filters, TeleBot
 
 
 def add_custom_filters(bot: TeleBot):
-    ...
+    bot.add_custom_filter(custom_filters.StateFilter(bot))
+    bot.add_custom_filter(custom_filters.IsDigitFilter())
