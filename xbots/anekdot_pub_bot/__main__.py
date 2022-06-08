e8587b7e2211055c83f289de6890f21ac3ce7275
Anton Shyshko <pozernishku@gmail.com>
2022-06-08 23:16:34 +0300

    feat: change default state_storage for bot: StateMemoryStorage -> StatePickleStorage

--- xbots/anekdot_pub_bot/__main__.py
+++ xbots/anekdot_pub_bot/__main__.py
@@ -1,13 +1,17 @@
 import os
 import threading
 
-from telebot import TeleBot
+from telebot import StatePickleStorage, TeleBot
 
 from xbots.anekdot_pub_bot.filters.common import add_custom_filters
 from xbots.anekdot_pub_bot.handlers.common import register_handlers, run_pending
 
 if __name__ == "__main__":
-    bot = TeleBot(os.environ["ANEKDOT_PUB_BOT_TOKEN"], num_threads=5)
+    bot = TeleBot(
+        os.environ["ANEKDOT_PUB_BOT_TOKEN"],
+        num_threads=5,
+        state_storage=StatePickleStorage(),
+    )
     register_handlers(bot)
     add_custom_filters(bot)
     threading.Thread(target=run_pending, name="run_pending", daemon=True).start()
