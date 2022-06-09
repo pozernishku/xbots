b68342028637cab90083c9b18bf50fd3d6a34b50
Anton Shyshko <pozernishku@gmail.com>
2022-06-09 13:23:50 +0300

    refactor: update add_custom_filters() -> returns bot instance

--- xbots/anekdot_pub_bot/__main__.py
+++ xbots/anekdot_pub_bot/__main__.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
         state_storage=StatePickleStorage(),
     )
     bot = register_handlers(bot)
-    add_custom_filters(bot)
+    bot = add_custom_filters(bot)
     threading.Thread(target=run_pending, name="run_pending", daemon=True).start()
     bot.infinity_polling(skip_pending=True)
--- xbots/anekdot_pub_bot/filters/common.py
+++ xbots/anekdot_pub_bot/filters/common.py
@@ -1,8 +1,9 @@
 from telebot import custom_filters, TeleBot
 
 
-def add_custom_filters(bot: TeleBot):
+def add_custom_filters(bot: TeleBot) -> TeleBot:
     bot.add_custom_filter(custom_filters.StateFilter(bot))
     bot.add_custom_filter(custom_filters.IsDigitFilter())
     # TODO: Add custom filter IsURL for channel URL checking
     # TODO: Add custom filter 'does bot is admin in your channel?'
+    return bot
