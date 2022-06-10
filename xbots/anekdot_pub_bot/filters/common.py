5054f475deea5b2f4a368894c42b157b24cf236f
Anton Shyshko <pozernishku@gmail.com>
2022-06-11 01:16:04 +0300

    feat: introduce ForwardFilter() in filters.common module

--- xbots/anekdot_pub_bot/filters/common.py
+++ xbots/anekdot_pub_bot/filters/common.py
@@ -4,6 +4,6 @@ from telebot import custom_filters, TeleBot
 def add_custom_filters(bot: TeleBot) -> TeleBot:
     bot.add_custom_filter(custom_filters.StateFilter(bot))
     bot.add_custom_filter(custom_filters.IsDigitFilter())
-    # TODO: Add custom filter IsURL for channel URL checking
+    bot.add_custom_filter(custom_filters.ForwardFilter())
     # TODO: Add custom filter 'does bot is admin in your channel?'
     return bot
