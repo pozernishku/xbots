b03a1e8082a5c09bb957f5d3fd987b2eefac56b7
Anton Shyshko <pozernishku@gmail.com>
2022-06-11 15:33:41 +0300

    feat: format pdf list to show it prettilly; add command tip; tidy up

--- xbots/anekdot_pub_bot/utilities.py
+++ xbots/anekdot_pub_bot/utilities.py
@@ -1,10 +1,20 @@
 def prepare_settings_message(data: dict) -> str:
     empty_value = "<пусто>"
+    pdf_list = data.get("pdf_list", [])
+    pdf_list_str = "\n".join([f"- {pdf_url}" for pdf_url in pdf_list]) or empty_value
+    pdf_list_str = "\n" + pdf_list_str if pdf_list_str != empty_value else pdf_list_str
     # TODO: Bold the values
     settings_msg = (
         f"Ваши настройки:\n"
         f"Канал: {data.get('channel', empty_value)}\n"
-        f"Периодичность: {data.get('periodicity', empty_value)}\n"
-        f"PDF-файлы: {data.get('pdf_list', empty_value)}"
+        f"Периодичность, мин: {data.get('periodicity', empty_value)}\n"
+        f"PDF-файлы: {pdf_list_str}"
     )
-    return settings_msg
+    tip_commands_message = "\n\n" + "\n".join(
+        [
+            "/start - изменить настройки",
+            "/delete - удалить настройки",
+            "/settings - просмотреть настройки",
+        ]
+    )
+    return settings_msg + tip_commands_message
