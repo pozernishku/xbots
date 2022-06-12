26e09a09803f255c853fda8ffef98e8d2158827f
Anton Shyshko <pozernishku@gmail.com>
2022-06-13 02:54:30 +0300

    feat: use activate_job_schedule_in_channels() in __main__.py

--- xbots/anekdot_pub_bot/__main__.py
+++ xbots/anekdot_pub_bot/__main__.py
@@ -5,6 +5,7 @@ from telebot import StatePickleStorage, TeleBot
 
 from xbots.anekdot_pub_bot.filters.common import add_custom_filters
 from xbots.anekdot_pub_bot.handlers.common import register_handlers, run_pending
+from xbots.anekdot_pub_bot.utilities import activate_job_schedule_in_channels
 
 if __name__ == "__main__":
     bot = TeleBot(
@@ -15,4 +16,5 @@ if __name__ == "__main__":
     bot = register_handlers(bot)
     bot = add_custom_filters(bot)
     threading.Thread(target=run_pending, name="run_pending", daemon=True).start()
+    bot = activate_job_schedule_in_channels(bot, debug_activate=True)
     bot.infinity_polling(skip_pending=True)
