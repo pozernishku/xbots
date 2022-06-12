0a48cdcd847cc3098f49774c1d072c51f7f3ba5f
Anton Shyshko <pozernishku@gmail.com>
2022-06-12 17:39:00 +0300

    feat: add custom filter IsCorrectPeriodicityFilter

--- xbots/anekdot_pub_bot/filters/common.py
+++ xbots/anekdot_pub_bot/filters/common.py
@@ -1,11 +1,13 @@
 from telebot import custom_filters, TeleBot
 
+from xbots.anekdot_pub_bot.filters.custom_filters import IsCorrectPeriodicityFilter
+
 PDF_URL_LIST_REGEX = r"(?i)https?://.+\.pdf"
 
 
 def add_custom_filters(bot: TeleBot) -> TeleBot:
     bot.add_custom_filter(custom_filters.StateFilter(bot))
-    bot.add_custom_filter(custom_filters.IsDigitFilter())
+    bot.add_custom_filter(IsCorrectPeriodicityFilter())
     bot.add_custom_filter(custom_filters.ForwardFilter())
     # TODO: Add custom filter 'does bot is admin in your channel?'
     return bot
